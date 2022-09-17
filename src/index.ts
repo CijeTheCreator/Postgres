d81d77cd29071c9fa3bc54f16038ee9d9a2852d0
David Ejere <ejeredavid2001@gmail.com>
2022-09-17 07:31:44 +0100

    Setup the database connection

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-console.log("Hello World!!!!!!!");
+import { createConnection } from "typeorm";
 
-let x: string = "Hello";
+const main = async () => {
+  try {
+    await createConnection({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "8563846a",
+      database: "typeorm-crash-course",
+    });
+    console.log("Connected to Postgres");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Unable to Connect to db");
+  }
+};
+
+main();
